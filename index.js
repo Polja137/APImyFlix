06cbc37e38be21ee893c837311078c0c359433b6
Polja137 <117524191+Polja137@users.noreply.github.com>
2023-02-15 20:33:51 +0100

    excercise 2.4

---
+++ index.js
@@ -0,0 +1,51 @@
+const express=require('express');
+const morgan=require('morgan');
+
+const app=express();
+app.use(morgan('common'));
+
+
+app.use(express.static('public'));
+
+let topMovies = [
+    {
+        title:'Inception',
+        director:'Christopher Nolan'
+    },
+    {
+        title:'Shutter island',
+        director:'Martin Scorsese'
+    },
+    {
+      title: 'Harry Potter and the Sorcerer\'s Stone',
+      director: 'J.K. Rowling'
+    },
+    {
+      title: 'Lord of the Rings',
+      director: 'J.R.R. Tolkien'
+    },
+    {
+      title: 'Twilight',
+      director: 'Stephanie Meyer'
+    }
+  ];
+  
+  // GET requests
+  app.get('/', (req, res) => {
+    res.send('Welcome to my movie API!');
+  });
+  
+  app.get('/documentation', (req, res) => {                  
+    res.sendFile('public/documentation.html', { root: __dirname });
+  });
+  
+  app.get('/movies', (req, res) => {
+    res.json(topMovies);
+  });
+  
+  
+
+  // listen for requests
+  app.listen(8080, () => {
+    console.log('Your app is listening on port 8080.');
+  });
